932cc34844fd860152e99af1390dbdfb089096cc
Александр Прокофьев <alex@MacBook-Pro-Aleksandr.local>
2024-11-17 13:14:21 +0300

    Fix after rev0.1

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,6 +1,5 @@
 from http import HTTPStatus
 
-from notes.forms import WARNING
 from notes.models import Note
 from pytils.translit import slugify
 
@@ -8,7 +7,6 @@ from .base import BaseTestCase
 
 
 class TestNoteEditDelete(BaseTestCase):
-
     def setUp(self):
         super().setUp()
         self.initial_title = self.note.title
@@ -16,14 +14,15 @@ class TestNoteEditDelete(BaseTestCase):
         self.initial_slug = self.note.slug
 
     def test_create_note_authorized(self):
-        Note.objects.all().delete()
         response = self.auth_client.post(
             self.add_url, data=self.form_data
         )
         self.assertRedirects(response, self.success_url)
         note = Note.objects.get()
-        self.assertEqual(note.title, self.TITLE)
-        self.assertEqual(note.content, self.form_data['content'])
+        self.assertEqual(note.title, self.form_data['title'])
+        self.assertEqual(note.text, self.form_data['text'])
+        self.assertEqual(note.slug, self.form_data['slug'])
+        self.assertEqual(note.author, self.auth_client.user)
 
     def test_create_note_unauthorized(self):
         Note.objects.all().delete()
@@ -38,7 +37,9 @@ class TestNoteEditDelete(BaseTestCase):
             response,
             form='form',
             field='slug',
-            errors=[WARNING]
+            errors=[
+                'такой slug уже существует, придумайте уникальное значение!'
+            ]
         )
         form = response.context['form']
         self.assertEqual(len(form.errors['slug']), 1)
@@ -48,17 +49,17 @@ class TestNoteEditDelete(BaseTestCase):
         response = self.auth_client.post(self.add_url, data=self.FORM_DATA_2)
         self.assertRedirects(response, self.success_url)
         generated_slug = slugify(self.FORM_DATA_2['title'])
-        self.assertTrue(Note.objects.filter(slug=generated_slug).exists())
+        self.assertEqual(Note.objects.get().slug, generated_slug)
 
     def test_author_can_edit(self):
-        self.assertRedirects(self.auth_client.post(
+        response = self.auth_client.post(
             self.edit_url, data=self.form_data
-        ), self.success_url)
-        self.note.refresh_from_db()
+        )
+        self.assertRedirects(response, self.success_url)
+        self.note = Note.objects.get(pk=self.note.pk)
         self.assertEqual(self.note.title, self.form_data['title'])
         self.assertEqual(self.note.text, self.form_data['text'])
         self.assertEqual(self.note.slug, self.form_data['slug'])
-        self.assertEqual(self.note.author, self.inital_author)
 
     def test_author_can_delete(self):
         initial_notes_count = Note.objects.count()
@@ -72,10 +73,11 @@ class TestNoteEditDelete(BaseTestCase):
         self.assertEqual(self.note.title, self.initial_title)
         self.assertEqual(self.note.text, self.initial_text)
         self.assertEqual(self.note.slug, self.initial_slug)
-        self.assertEqual(self.note.author, self.initial_author)
 
     def test_another_user_cannot_edit(self):
-        response = self.another_client.post(self.edit_url, data=self.form_data)
+        response = self.another_client.post(
+            self.edit_url, data=self.form_data
+        )
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         self.note_not_updated()
 
